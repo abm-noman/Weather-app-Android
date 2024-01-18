91678300fbe7c8746642b321f5a15e0aa654b7a9
abm-noman <noman15-2839@diu.edu.bd>
2024-01-19 01:10:46 +0600

    Dashboard Activity UI

---
+++ app/src/main/java/com/example/weather/Adapters/HourlyAdapters.java
@@ -0,0 +1,40 @@
+package com.example.weather.Adapters;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.weather.Domains.Hourly;
+
+import java.util.ArrayList;
+
+public class HourlyAdapters extends RecyclerView.Adapter<HourlyAdapters.viewholder> {
+    ArrayList<Hourly> items;
+    Context context;
+
+    public HourlyAdapters(ArrayList<Hourly> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public HourlyAdapters.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context= parent.getContext();
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HourlyAdapters.viewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewholder {
+    }
+}
